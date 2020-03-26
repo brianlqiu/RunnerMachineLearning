5141fd0f2b64cf5252292a67e1a8ea6cb9ba039e
Brian Qiu <brianlqiu5@gmail.com>
2020-03-26 00:02:48 -0700

    added jump animations

--- runner.py
+++ runner.py
@@ -7,8 +7,8 @@ WIN_HEIGHT = 720
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Runner")
 
-slide = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-slide-0{frame}.png"), (100, 74)) for frame in range(0, 2)]
-jump = [pygame.image.load(f"assets/adventurer-jump-0{frame}.png") for frame in range(0, 6)]
+slide = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-slide-0{frame}.png"), (200, 148)) for frame in range(0, 2)]
+jump = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-jump-0{frame}.png"), (200, 148)) for frame in range(0, 6)]
 run = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-run-0{frame}.png"), (200, 148)) for frame in range(0, 6)]
 bg = pygame.image.load('assets/Background.png')
 bg = pygame.transform.scale(bg, (1280, 720))
@@ -21,6 +21,7 @@ x = 300
 y = WIN_HEIGHT - height - 80
 vel = 20
 
+is_run = True
 is_jump = False
 jump_count = 8
 
@@ -28,15 +29,23 @@ distance = 0
 
 is_slide = False
 
-run_count = 0
+run_frame = 0
+jump_frame = 0
 
 def redraw_game_window():
-    global run_count
+    global run_frame
+    global jump_frame
     win.blit(bg, (0,0))
-    if run_count + 1 >= 18:
-        run_count = 0
-    win.blit(run[run_count//3], (x,y))
-    run_count += 1
+    if is_run: 
+        if run_frame + 1 >= 18:
+            run_frame = 0
+        win.blit(run[run_frame//3], (x,y))
+        run_frame += 1
+    if is_jump:
+        if jump_frame + 1 >= 18:
+            jump_frame = 0
+        win.blit(jump[jump_frame//3], (x,y))
+        jump_frame += 1
     pygame.display.update()
 
 
@@ -50,6 +59,7 @@ while running:
     keys = pygame.key.get_pressed()
     if not(is_jump) and keys[pygame.K_SPACE]:
         is_jump = True
+        is_run = False
     elif is_jump:
         if jump_count >= -8:
             neg = 1
@@ -59,6 +69,7 @@ while running:
             jump_count -= 1
         else:
             is_jump = False
+            is_run = True
             jump_count = 8
     redraw_game_window()
 
