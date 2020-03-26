be6960ea3a91edc416ff87987dac44e18f721146
Brian Qiu <brianlqiu5@gmail.com>
2020-03-25 22:28:06 -0700

    implemented jumping

--- runner.py
+++ runner.py
@@ -14,6 +14,9 @@ x = 0
 y = WIN_HEIGHT - height
 vel = 20
 
+is_jump = False
+jump_count = 5
+
 run = True
 while run:
     pygame.time.delay(50)
@@ -32,17 +35,33 @@ while run:
             x = WIN_WIDTH - width
         else: 
             x += vel
+    """
     if keys[pygame.K_UP]:
         if y < vel:
             y = 0
         else:
             y -= vel
+    """
+    
+    if not(is_jump) and keys[pygame.K_SPACE]:
+        is_jump = True
+    elif is_jump:
+        if jump_count >= -5:
+            neg = 1
+            if(jump_count < 0):
+                neg = -1
+            y -= (jump_count ** 2) * neg
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 5
+    """
     if keys[pygame.K_DOWN]:
         if y > WIN_HEIGHT - height - vel:
             y = WIN_HEIGHT - height
         else:
             y += vel
-
+    """
 
     win.fill((0, 0, 0))
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
