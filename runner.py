3945776e9b265446bd04dbf04fe64090c300a85d
Brian Qiu <brianlqiu5@gmail.com>
2020-03-26 05:18:32 -0700

    added hitboxes for character

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 pygame.init()
 
 
@@ -13,7 +14,7 @@ jump = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-jump-0{fram
 run = [pygame.transform.scale(pygame.image.load(f"assets/adventurer-run-0{frame}.png"), (200, 148)) for frame in range(0, 6)]
 bg = pygame.transform.scale(pygame.image.load('assets/Background.png'), (1280, 720))
 axe = pygame.transform.scale(pygame.image.load("assets/groundaxe.png"), (60, 59))
-sign = pygame.image.load("assets/sign.png")
+sign = pygame.transform.scale(pygame.image.load("assets/sign.png"), (100, 157))
 bg_x = 0
 bg_x2 = bg.get_width()
 
@@ -33,20 +34,24 @@ class player(object):
         self.run_frame = 0
         self.jump_frame = 0
         self.slide_frame = 0
+        self.hitbox = (self.x + 80, self.y + 30, 90, 115)
 
     def draw(self, win):
         if self.is_run: 
+            self.hitbox = (self.x + 80, self.y + 30, 70, 115)
             if self.run_frame + 1 >= 18:
                 self.run_frame = 0
             win.blit(run[self.run_frame // 3], (self.x, self.y))
             self.run_frame += 1
         if self.is_jump:
+            self.hitbox = (self.x + 80, self.y + 30, 70, 100)
             if self.jump_frame + 1 >= 18:
                 self.jump_frame = 0
             win.blit(jump[self.jump_frame // 5], (self.x, self.y))
             self.jump_frame += 1
             self.run_frame = 0
         if self.is_slide:
+            self.hitbox = (self.x + 50, self.y + 80, 95, 60)
             if self.slide_frame + 1 >= 6:
                 self.slide_frame = 0
             win.blit(slide[self.slide_frame // 3], (self.x, self.y))
@@ -64,24 +69,32 @@ class obstacle(object):
     def draw(self, win):
         win.blit(self.obst, (self.x, self.y))
 
-        
-
 
 def redraw_game_window():
     win.blit(bg, (bg_x, 0))
     win.blit(bg, (bg_x2, 0))
     char.draw(win)
-    test_obst.draw(win)
+    for obst in obstacles:
+        obst.draw(win)
     pygame.display.update()
 
 
 running = True
 char = player(300, WIN_HEIGHT - 228, 200, 148)
-test_obst = obstacle(WIN_WIDTH, WIN_HEIGHT - 139, 60, 59, axe)
-airtime = 0
+obstacles = []
+pygame.time.set_timer(pygame.USEREVENT+2, random.randrange(610, 1000))
 while running:
     #frame rate
-    clock.tick(60)
+    clock.tick(30)
+
+    #rules for obstacle generation
+    #8 frames to reach peak jump
+    #9 frames to return to ground
+    #0 frames to start sliding
+    #ground -> ground = distance of at least 17 frames
+    #ground -> air = distance of at least 9 frames
+    #air -> ground = distance of at least 8 frames
+    #air -> air = distance of at least 0 frames
 
     #scrolling screen
     bg_x -= 10
@@ -90,7 +103,7 @@ while running:
         bg_x = bg.get_width()
     if bg_x2 < bg.get_width() * -1:
         bg_x2 = bg.get_width()
-    test_obst.x -= 10
+    char.distance += 1
 
     #exit & slide end listeners
     for event in pygame.event.get():
@@ -99,6 +112,16 @@ while running:
         elif event.type == pygame.KEYUP and char.is_slide:
             char.is_slide = False
             char.is_run = True
+        elif event.type == pygame.USEREVENT+2:
+            rand = random.randint(0, 1)
+            if rand == 0:
+                obstacles.append(obstacle(WIN_WIDTH, WIN_HEIGHT - 139, 60, 59, axe))
+            else:
+                obstacles.append(obstacle(WIN_WIDTH, WIN_HEIGHT - 239, 60, 59, sign))
+
+
+    for obst in obstacles:
+        obst.x -= 10
 
     #player input
     keys = pygame.key.get_pressed()
@@ -108,14 +131,12 @@ while running:
         char.is_slide = False
     elif char.is_jump:
         if char.jump_count >= -8:
-            airtime += 1
             neg = 1
             if(char.jump_count < 0):
                 neg = -1
             char.y -= (char.jump_count ** 2) * neg * 0.5
             char.jump_count -= 1
         else:
-            print(airtime)
             char.is_jump = False
             char.is_run = True
             char.jump_count = 8
